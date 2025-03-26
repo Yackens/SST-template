738f8914a4aa3f7044e932a9983431950ff98a36
Joaquin Maroto <joaquin.maroto@ansys.com>
2025-03-26 20:55:50 +0100

    Version 1 of Git file

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,1 +1,11 @@
-export const bucket = new sst.aws.Bucket("MyBucket");
+// Create an S3 bucket
+export const bucket = new sst.aws.Bucket("Uploads");
+
+// Create the DynamoDB table
+export const table = new sst.aws.Dynamo("One-Peak", {
+    fields: {
+      userId: "string",
+      tenantId: "string",
+    },
+    primaryIndex: { hashKey: "userId", rangeKey: "tenantId" },
+  });
